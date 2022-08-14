cb70733e85518cc6a6fae0710c4f374b4f5fb2ca
Rohit Garg <rohit.garg@meesho.com>
2022-08-14 18:22:30 +0530

    Initial commit

---
+++ src/main/java/com/example/libraryeventsproducer/domain/Book.java
@@ -0,0 +1,18 @@
+package com.example.libraryeventsproducer.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Book {
+    private Integer bookId;
+    private String bookName;
+    private String bookAuthor;
+
+
+}
---
+++ src/main/java/com/example/libraryeventsproducer/domain/LibraryEvent.java
@@ -0,0 +1,17 @@
+package com.example.libraryeventsproducer.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class LibraryEvent {
+
+    private Integer LibraryEventId;
+    private Book book;
+
+}
